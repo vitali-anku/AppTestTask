224ac662d3216f077d844be03e1263f54d6eba74
vitali <vitalja.ja@gmail.com>
2018-12-19 19:42:48 +0300

    add Comletable

--- app/src/main/java/com/testtask/apptesttask/model/interactor/characters/CharactersInteractor.kt
+++ app/src/main/java/com/testtask/apptesttask/model/interactor/characters/CharactersInteractor.kt
@@ -1,16 +1,19 @@
 package com.testtask.apptesttask.model.interactor.characters
 
 import com.testtask.apptesttask.entity.charactrers.CharacterDataWrapper
-import io.reactivex.Observable
+import io.reactivex.Completable
 import io.reactivex.Single
 
 class CharactersInteractor {
 
     fun getCharacters(): Single<CharacterDataWrapper> {
-        return TODO("characters interactor (Add implementation returned value method).")
+        TODO("characters interactor (Add implementation returned value method).")
     }
 
-    fun setFavoritesCharacters(id: Int): Observable<CharacterDataWrapper> {
-        return TODO("characters interactor (Add implementation returned value method).")
+    fun setFavoritCharacter(id: Int, like: Boolean): Completable {
+        if (like)
+            return Completable.complete()
+        else
+            return Completable.error(Throwable())
     }
 }
