c7b2acf8ffa2d3234c9ce0de0f02c1b43e47aa56
vitali <vitalja.ja@gmail.com>
2018-12-19 19:09:35 +0300

    removed param by method interactor

--- app/src/main/java/com/testtask/apptesttask/model/interactor/characters/CharactersInteractor.kt
+++ app/src/main/java/com/testtask/apptesttask/model/interactor/characters/CharactersInteractor.kt
@@ -2,7 +2,7 @@ package com.testtask.apptesttask.model.interactor.characters
 
 class CharactersInteractor {
 
-    fun getCharacters(ts: String, apikey: String, hash: String) {
+    fun getCharacters() {
         //TODO characters interactor (Add implementation type and returned value method).
     }
 
