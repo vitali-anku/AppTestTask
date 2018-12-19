68488614d85d2b5fbbf642a4acf569e5163c1db6
vitali <vitalja.ja@gmail.com>
2018-12-19 19:22:15 +0300

    add type methods and RxJava

--- app/src/main/java/com/testtask/apptesttask/model/interactor/characters/CharactersInteractor.kt
+++ app/src/main/java/com/testtask/apptesttask/model/interactor/characters/CharactersInteractor.kt
@@ -1,12 +1,16 @@
 package com.testtask.apptesttask.model.interactor.characters
 
+import com.testtask.apptesttask.entity.charactrers.CharacterDataWrapper
+import io.reactivex.Observable
+import io.reactivex.Single
+
 class CharactersInteractor {
 
-    fun getCharacters() {
-        //TODO characters interactor (Add implementation type and returned value method).
+    fun getCharacters(): Single<CharacterDataWrapper> {
+        return TODO("characters interactor (Add implementation returned value method).")
     }
 
-    fun setFavoritesCharacters(id: Int) {
-        //TODO characters interactor (Add implementation type and returned value method).
+    fun setFavoritesCharacters(id: Int): Observable<CharacterDataWrapper> {
+        return TODO("characters interactor (Add implementation returned value method).")
     }
 }
